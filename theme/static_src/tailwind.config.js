5073d51adab12fe7ed0176b00e4bf1b5bfdd3bee
BrahimCodeA <daoudi.ibrahimd@gmail.com>
2024-03-01 14:32:19 +0100

    fin

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -1,43 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-
-
-  mode: "jit",
-
-  content: [
+    mode: "jit",
+    content: [
       '../../**/templates/**/*.html',
-  ],
-  theme: {
+    ],
+    theme: {
       extend: {
-          textColor: {
-              'green': 'rgb(27, 42, 38)',
-              'brown': 'rgb(242,212,53)',
-              'violet': 'rgb(10,16,53)',
-          },
-          backgroundColor: {
-              'lime': 'rgb(38, 105, 89)',
-
-          },
-          boxShadow: {
-              '0_9px_0_rgb(0,0,0)': '0 9px 0 rgba(0, 0, 0, 0.1)',
-              '0_4px_0px_rgb(0,0,0)': '0 4px 0 rgba(0, 0, 0, 0.1)',
-          },
-          fontFamily: {
-              'font-Viga': ['Viga', 'sans-serif'],
-              'font-IM':['IM Fell English SC', 'serif'],
-              'bungee': ['Bungee Spice', 'cursive'],
-              'im-fell': ['IM Fell English SC', 'serif'],
-              'paytone': ['Paytone One', 'sans-serif'],
-            },
-          },   
-          screens: {
-              'sm': '740px',  // Personnalisez la taille pour sm comme vous le souhaitez
-              'md': '1000px',
-              'lg': '1600px',
-              'xl': '2280px',
-            },
-          },
+        textColor: {
+          'green': 'rgb(27, 42, 38)',
+          'brown': 'rgb(242,212,53)',
+          'violet': 'rgb(10,16,53)',
+        },
+        backgroundColor: {
+          'lime': 'rgb(38, 105, 89)',
+        },
+        boxShadow: {
+          '0_9px_0_rgb(0,0,0)': '0 9px 0 rgba(0, 0, 0, 0.1)',
+          '0_4px_0px_rgb(0,0,0)': '0 4px 0 rgba(0, 0, 0, 0.1)',
+        },
+        fontFamily: {
+          'font-Viga': ['Viga', 'sans-serif'],
+          'font-IM':['IM Fell English SC', 'serif'],
+          'bungee': ['Bungee Spice', 'cursive'],
+          'im-fell': ['IM Fell English SC', 'serif'],
+          'paytone': ['Paytone One', 'sans-serif'],
+        },
       },
-  {}
+      screens: {
+        'sm': '740px',  // Personnalisez la taille pour sm comme vous le souhaitez
+        'md': '1000px',
+        'lg': '1600px',
+        'xl': '2280px',
+      },
+    },
+    variants: {
+      extend: {
+        overflowX: ['responsive', 'hover'], // Ajout de la propriété overflowX
+      },
+    },
+    plugins: [],
+  }
   
